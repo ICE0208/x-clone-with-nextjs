419410a1f5c48c3eb5159aa427fcc9e86ef860f4
ICE0208 <doky3307@naver.com>
2023-11-08 10:45:58 +0900

    btn에 onClick 속성 추가

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,27 @@
 import { cls } from "@/lib/client/utils";
+import { MouseEvent } from "react";
 
 interface ButtonProps {
   text?: string;
   kind: "blue-white" | "white-black" | "transparent-blue" | "transparent-white";
+  onClick?: (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => void;
   [key: string]: any;
 }
 
-export default function Button({ text = "", kind, ...rest }: ButtonProps) {
+export default function Button({
+  text = "",
+  kind,
+  onClick,
+  ...rest
+}: ButtonProps) {
   const DEFAULT_CLASS =
-    "flex h-[38px] w-full items-center justify-center rounded-3xl text-sm";
+    "flex h-[38px] w-full items-center justify-center rounded-3xl text-sm cursor-pointer";
 
   return (
     <>
       {kind === "white-black" && (
         <div
+          onClick={onClick}
           className={cls(DEFAULT_CLASS, "bg-white font-semibold text-black")}
           {...rest}
         >
@@ -22,6 +30,7 @@ export default function Button({ text = "", kind, ...rest }: ButtonProps) {
       )}
       {kind === "blue-white" && (
         <div
+          onClick={onClick}
           className={cls(DEFAULT_CLASS, "bg-[#1D9BF1] font-bold text-white")}
           {...rest}
         >
@@ -30,6 +39,7 @@ export default function Button({ text = "", kind, ...rest }: ButtonProps) {
       )}
       {kind === "transparent-blue" && (
         <div
+          onClick={onClick}
           className={cls(
             DEFAULT_CLASS,
             "border-[1px] border-[#546571] bg-transparent text-base font-extrabold text-[#1D9BF1]",
@@ -41,6 +51,7 @@ export default function Button({ text = "", kind, ...rest }: ButtonProps) {
       )}
       {kind === "transparent-white" && (
         <div
+          onClick={onClick}
           className={cls(
             DEFAULT_CLASS,
             "border-[1px] border-[#546571] bg-transparent text-base font-extrabold text-[white]",
