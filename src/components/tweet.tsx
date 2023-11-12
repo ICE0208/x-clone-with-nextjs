ae8951f6d2b65e83dd10a39c5f6b6209d1d935ad
ICE0208 <doky3307@naver.com>
2023-11-13 00:20:17 +0900

    게시글 클릭 범위 수정

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -23,10 +23,7 @@ export default function TweetPreview({ tweet }: { tweet: TweetWithDetail }) {
   }, [tweet]);
 
   return (
-    <div
-      key={tweet.id}
-      className="scrollbar-hide test-white box-border w-full resize-none rounded-2xl border border-white bg-black p-4"
-    >
+    <div>
       <Link
         href={{
           pathname: `/tweets/${tweet.id}`,
@@ -34,35 +31,42 @@ export default function TweetPreview({ tweet }: { tweet: TweetWithDetail }) {
         }}
         as={`/tweets/${tweet.id}`}
       >
-        <div className="flex flex-col space-x-2">
-          <div className="flex space-x-2 text-lg">
-            <div>🐹</div>
-            <span>{tweet.user.name}</span>
-          </div>
-          <hr className="my-3" />
-          <div
-            ref={boxRef}
-            className={`scrollbar-hide overflow max-h-[170px] flex-grow overflow-hidden`}
-          >
-            <div ref={textRef}>
-              {tweet.text.split("\n").map((line, index) => (
-                <React.Fragment key={index}>
-                  <p className="break-all">{line}</p>
-                  <br />
-                </React.Fragment>
-              ))}
+        <div
+          key={tweet.id}
+          className="scrollbar-hide test-white box-border w-full resize-none rounded-2xl border border-white bg-black p-4"
+        >
+          <div className="flex flex-col space-x-2">
+            <div className="flex space-x-2 text-lg">
+              <div>🐹</div>
+              <span>{tweet.user.name}</span>
             </div>
-          </div>
-          {isOverflow ? (
-            <div className="mt-2 flex justify-center">⋮</div>
-          ) : null}
-          <div className="text-sm">
-            <div className="mt-4 text-gray-400">
-              <span>
-                {format(new Date(tweet.createdAt), `a HH:mm · yyyy-MM-dd`)}
+            <hr className="my-3" />
+            <div
+              ref={boxRef}
+              className={`scrollbar-hide overflow max-h-[170px] flex-grow overflow-hidden`}
+            >
+              <div ref={textRef}>
+                {tweet.text.split("\n").map((line, index) => (
+                  <React.Fragment key={index}>
+                    <p className="break-all">{line}</p>
+                    <br />
+                  </React.Fragment>
+                ))}
+              </div>
+            </div>
+            {isOverflow ? (
+              <div className="mt-2 flex justify-center">⋮</div>
+            ) : null}
+            <div className="text-sm">
+              <div className="mt-4 text-gray-400">
+                <span>
+                  {format(new Date(tweet.createdAt), `a HH:mm · yyyy-MM-dd`)}
+                </span>
+              </div>
+              <span className=" text-gray-400">
+                Likes: {tweet._count.likes}
               </span>
             </div>
-            <span className=" text-gray-400">Likes: {tweet._count.likes}</span>
           </div>
         </div>
       </Link>
